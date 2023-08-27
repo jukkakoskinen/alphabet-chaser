6e27b8f13fc68a764f14825bc5372bd5463a4c06
Pete Cooke <peterjamescooke@gmail.com>
2023-08-27 13:27:41 +0300

    Initial commit. - Canvas and context setup. - Score added to canvas.

---
+++ src/components/MainCanvas.vue
@@ -0,0 +1,93 @@
+<template>
+    <div>
+        <canvas id="mainCanvas"></canvas>
+        <div>
+            <ion-button>Left</ion-button>
+            <ion-button>Right</ion-button>
+            <ion-button>Up</ion-button>
+            <ion-button>Down</ion-button>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { IonButton } from '@ionic/vue';
+import { ref, onMounted } from 'vue';
+
+const Direction = {
+    Left: 0,
+    Right: 1,
+    Up: 2,
+    Down: 3
+}
+
+function Alphabet(canvasContext) {
+    this.speed = 10;
+    this.letter = {};
+
+    this.init = function () {
+        canvasContext.font = '20px Comic Sans MS';
+        this.tail = [{}];
+        this.direction = Direction.Right;
+        this.score = 0;
+        this.gameOver = false;
+        this.willEat = false;
+    }
+
+    this.startGame = () => {
+        this.init();
+        //this.move();
+        //this.letterGenerator();
+    }
+
+    this.setDirection = (direction) => {
+        this.direction = direction;
+    }
+
+    this.draw = () => {
+        console.log(canvasContext)
+        canvasContext.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+
+        //draw score
+        canvasContext.fillStyle = 'black';
+        canvasContext.fillText('Score: ' + this.score, 10, 25);
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+onMounted(() => {
+    const mainCanvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('mainCanvas');
+    const canvasContext = mainCanvas.getContext('2d');
+
+    let game = new Alphabet(canvasContext);
+    game.startGame();
+    game.draw();
+
+
+});
+
+
+
+
+
+</script>
+
+<style>
+#mainCanvas {
+    width: 300px;
+    height: 500px;
+    border: 2px solid gray;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,34 +1,27 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <div>
+          <strong>Alphabet Chaser</strong>
+        </div>
+        <div>
+          <main-canvas></main-canvas>
+        </div>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonPage } from '@ionic/vue';
+import mainCanvas from '@/components/MainCanvas.vue';
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -44,9 +37,9 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
