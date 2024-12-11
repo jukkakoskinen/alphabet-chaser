7bddb4b02659b69f12142f034b37234b97670785
Pete Cooke <peterjamescooke@gmail.com>
2024-12-11 13:00:38 +0200

    Fix to vite config for vitest. Added proxy for backend. package lock updates.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +18,15 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    environment: 'jsdom'
-  }
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'cypress/**/*'],
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Your backend server
+        changeOrigin: true,
+      },
+    },
+  },
 })
